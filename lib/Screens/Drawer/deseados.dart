397c4e6766423a8d65d71f4795bc8121b4dd6a79
Asbem20 <asbem2024@gmail.com>
2025-07-13 15:20:57 -0500

    Update deseados.dart
    Agregando anotaciones

--- lib/Screens/Drawer/deseados.dart
+++ lib/Screens/Drawer/deseados.dart
@@ -4,20 +4,22 @@ import 'package:cineqay/Screens/datolibro.dart';
 import 'package:flutter/material.dart';
 import 'package:cineqay/listas.dart';
 
+// Widget que muestra la lista de películas deseadas por el usuario
 class Deseados extends StatelessWidget {
-  final Map<String, dynamic> usuario;
+  final Map<String, dynamic> usuario; // Mapa con los datos del usuario
 
   const Deseados({super.key, required this.usuario});
 
   @override
   Widget build(BuildContext context) {
-    // Filtrar peliculas deseadas del usuario
+    // Filtra peliculas deseadas del usuario
     final List<Map<String, dynamic>> deseadosUsuario =
         peliculas.where((pelicula) {
+          // Verifica que la lista deseados exista y contenga el ID del usuario
           return pelicula['deseados'] != null &&
               pelicula['deseados'].contains(usuario['idusuario']);
         }).toList();
-
+    // Si no tiene películas deseadas, muestra un mensaje
     return deseadosUsuario.isEmpty
         ? const Center(
           child: Text(
@@ -25,25 +27,28 @@ class Deseados extends StatelessWidget {
             style: TextStyle(color: Appcolors.textPrimary),
           ),
         )
+        // Sino, muestra la lista de las películas deseadas
         : ListView.builder(
           padding: const EdgeInsets.all(12),
           itemCount: deseadosUsuario.length,
           itemBuilder: (context, index) {
-            final pelicula = deseadosUsuario[index];
+            final pelicula = deseadosUsuario[index]; // Película actual
             return ListTile(
+              // Imagen de la película
               leading: Image.network(
                 pelicula['img_url'],
                 width: 50,
                 height: 75,
                 fit: BoxFit.cover,
               ),
-              title: Text(pelicula['Titulo'], style: AppTexts.general),
-              subtitle: Text(pelicula['Genero'], style: AppTexts.subgeneral),
+              title: Text(pelicula['Titulo'], style: AppTexts.general), // Titulo
+              subtitle: Text(pelicula['Genero'], style: AppTexts.subgeneral), // Género
+              // Ícono botón para navegar a la página del respectivo libro
               trailing: IconButton(
                 icon: const Icon(Icons.arrow_forward_ios),
                 color: Appcolors.textPrimary,
                 onPressed: () {
-                  // Navegar a la pantalla de detalle de la película
+                  // Navega a la pantalla de detalle de la película
                   Navigator.push(
                     context,
                     MaterialPageRoute(

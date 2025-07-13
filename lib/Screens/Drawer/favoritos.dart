5bfbef45abf34c4b8e931bac3c8d07f545b545b8
Asbem20 <asbem2024@gmail.com>
2025-07-13 15:27:03 -0500

    Update favoritos.dart
    Agregando anotaciones

--- lib/Screens/Drawer/favoritos.dart
+++ lib/Screens/Drawer/favoritos.dart
@@ -4,20 +4,22 @@ import 'package:cineqay/Screens/datolibro.dart';
 import 'package:flutter/material.dart';
 import 'package:cineqay/listas.dart';
 
+// Widget que muestra una lista con las películas favoritos del usuario
 class Favoritos extends StatelessWidget {
-  final Map<String, dynamic> usuario;
+  final Map<String, dynamic> usuario; // Mapa con los datos del usuario actual
 
   const Favoritos({super.key, required this.usuario});
 
   @override
   Widget build(BuildContext context) {
-    // Filtrar películas favoritas del usuario
+    // Filtra las películas favoritas del usuario
     final List<Map<String, dynamic>> favoritosUsuario =
         peliculas.where((pelicula) {
+          // Verifica que la lista favoritos exista y contenga el ID del usuario
           return pelicula['favoritos'] != null &&
               pelicula['favoritos'].contains(usuario['idusuario']);
         }).toList();
-
+    // Si no tiene películas favoritas, muestra un mensaje
     return favoritosUsuario.isEmpty
         ? const Center(
           child: Text(
@@ -25,19 +27,22 @@ class Favoritos extends StatelessWidget {
             style: TextStyle(color: Appcolors.textPrimary),
           ),
         )
+        // Sino, muestra una lista con las películas favoritas
         : ListView.builder(
           itemCount: favoritosUsuario.length,
           itemBuilder: (context, index) {
-            final pelicula = favoritosUsuario[index];
+            final pelicula = favoritosUsuario[index]; // Película actual
             return ListTile(
+              // Imagen de la película
               leading: Image.network(
                 pelicula['img_url'],
                 width: 50,
                 height: 75,
                 fit: BoxFit.cover,
               ),
-              title: Text(pelicula['Titulo'], style: AppTexts.general),
-              subtitle: Text(pelicula['Genero'], style: AppTexts.subgeneral),
+              title: Text(pelicula['Titulo'], style: AppTexts.general), // Titulo
+              subtitle: Text(pelicula['Genero'], style: AppTexts.subgeneral), // Género
+              // Ícono botón para navegar a la página del respectivo libro
               trailing: IconButton(
                 icon: const Icon(Icons.arrow_forward_ios),
                 color: Appcolors.textPrimary,

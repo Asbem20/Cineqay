4bbc444311b3724cec32d679d32265fe23189dfb
Asbem20 <asbem2024@gmail.com>
2025-07-13 15:14:13 -0500

    Update cuenta.dart
    Agregando anotaciones

--- lib/Screens/Drawer/cuenta.dart
+++ lib/Screens/Drawer/cuenta.dart
@@ -3,18 +3,13 @@ import 'package:cineqay/Disenos/app_colors.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-class Cuenta extends StatefulWidget {
-  final Map<String, dynamic> usuario; // <== Agregado
+// Widget que muestra la información del usuario actual
+class Cuenta extends StatelessWidget {
+  final Map<String, dynamic> usuario;
 
-  const Cuenta({super.key, required this.usuario}); // <== Modificado
+  const Cuenta({super.key, required this.usuario});
 
-  @override
-  State<Cuenta> createState() => _CuentaState();
-}
-
-class _CuentaState extends State<Cuenta> {
-  
-  // Formatea la fecha
+  // Formatea la fecha tipo 'YYYY-MM-DD' a 'DD/MM/YYYY'
   static String _formatearFecha(String fechaISO) {
     final partes = fechaISO.split('-');
     return '${partes[2]}/${partes[1]}/${partes[0]}';
@@ -22,44 +17,42 @@ class _CuentaState extends State<Cuenta> {
 
   @override
   Widget build(BuildContext context) {
-    final usuarioActual = widget.usuario; // <== Usa el usuario pasado
+    final usuarioActual = usuario; // Acede a los datos del usuario
 
     return SingleChildScrollView(
-      scrollDirection: Axis.vertical,
       child: Padding(
         padding: const EdgeInsets.all(20),
         child: Column(
           children: [
             const SizedBox(height: 40),
-
+            // Avatar circular del perfil
             const CircleAvatar(
               radius: 70,
               backgroundColor: Appcolors.secondary,
               child: Icon(Icons.person, size: 70, color: Appcolors.textPrimary),
             ),
-
             const SizedBox(height: 20),
-
+            // Widgets personalizados para el diseño de los contenedores
             ItemProfile(
-              title: 'Nombres',
+              title: 'Nombres', // Nombres
               subtitle: usuarioActual['nombre'],
               iconData: CupertinoIcons.person,
             ),
             const SizedBox(height: 10),
             ItemProfile(
-              title: 'Apellidos',
+              title: 'Apellidos', // Apellidos
               subtitle: usuarioActual['apellido'],
               iconData: CupertinoIcons.person,
             ),
             const SizedBox(height: 10),
             ItemProfile(
-              title: 'Fecha de nacimiento',
+              title: 'Fecha de nacimiento', // Fecha de Nacimiento
               subtitle: _formatearFecha(usuarioActual['fechanaci']),
               iconData: CupertinoIcons.calendar,
             ),
             const SizedBox(height: 10),
             ItemProfile(
-              title: 'Email',
+              title: 'Email', // Email
               subtitle: usuarioActual['email'],
               iconData: CupertinoIcons.mail,
             ),
